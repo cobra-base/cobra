48573a34b66981f2bbb371d0962587b9dc04f3c3
lixg <doingli@foxmail.com>
2024-09-03 18:21:40 +0800

    1

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 const time = require('./lib/util/time')
 const rand = require('./lib/util/rand')
 
-module.exports.util = {
-    time: time,
-    rand: rand,
-}
+module.exports.time = time
+module.exports.rand = rand
 
 module.exports.putMsg = function() {
     console.warn('Great! you got')
