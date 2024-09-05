901938b0ec585617f28e875a99a3a8123510979e
lixg <doingli@foxmail.com>
2024-09-06 00:53:22 +0800

    1

--- index.js
+++ index.js
@@ -2,11 +2,13 @@
 
 const time = require('./lib/util/time')
 const rand = require('./lib/util/rand')
+const func = require('./lib/util/func')
 const erc20 = require('./lib/erc20/erc20')
 const uniswap = require('./lib/swap/uniswap')
 
 module.exports.time = time
 module.exports.rand = rand
+module.exports.func = func
 module.exports.erc20 = erc20
 module.exports.uniswap = uniswap
 
